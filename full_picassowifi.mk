fdc9c508eebb67b6ceaa5d3d8677f72ba9b212bd
Christopher R. Palmer <crpalmer@gmail.com>
2014-09-05 20:13:23 -0400

    picassowifi: Don't include telephony
    We still need to clean up the overlays, but get rid of the extra
    telephony in the includes.

--- full_picassowifi.mk
+++ full_picassowifi.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit from picassowifi device
 $(call inherit-product, device/samsung/picassowifi/device.mk)
