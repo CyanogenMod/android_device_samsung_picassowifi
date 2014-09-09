f73bec7a81998dcf8d89858aa3df32a7436df2bc
Christopher R. Palmer <crpalmer@gmail.com>
2014-09-09 05:54:27 -0400

    picassowifi: Add tcpdump

--- device.mk
+++ device.mk
@@ -96,6 +96,12 @@ PRODUCT_PACKAGES += \
     libgdmcprov \
     mcDriverDaemon
 
+# Network tools
+
+PRODUCT_PACKAGES += \
+    libpcap \
+    tcpdump
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
