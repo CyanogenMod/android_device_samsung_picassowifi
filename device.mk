f77cc3979a1aad772060e853d7fc06c9f0cea5ca
Christopher R. Palmer <crpalmer@gmail.com>
2014-09-23 06:36:59 -0400

    picassowifi: consumerir: Kang from i9500

--- device.mk
+++ device.mk
@@ -65,7 +65,7 @@ PRODUCT_PACKAGES += \
 
 # IR
 PRODUCT_PACKAGES += \
-    consumerir.exynos5
+    consumerir.universal5420
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
@@ -112,6 +112,7 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
