d94d560edaecec1a1a8d67f1b41908eef9c1f6fa
Christopher R. Palmer <crpalmer@gmail.com>
2014-10-05 05:42:12 -0400

    picassowifi: power: Flash buttons to show power on event
    Our display turns on REALLY slowly, use the capacitive buttons to
    provide some kind of feedback to indicate that power has been
    applied and sooner or later you should see the screen...
    
    Change-Id: Ice712f229a0248accc653735787f177a2eee6158

--- power/power.c
+++ power/power.c
@@ -25,12 +25,55 @@
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
+#define BUTTONS_FLASH_MS   5
+#define BUTTONS_BRIGHTNESS "/sys/devices/virtual/sec/sec_touchkey/brightness"
+
+/* Remember old state of the interactive flag (which is on on boot) to avoid
+ * flashing the button lights more than 1 and to avoid flashing them on boot.
+ */
+static int old_interactive_state = 1;
+
+static void sysfs_write(char *path, char *s) {
+    char buf[80];
+    int len;
+    int fd = open(path, O_WRONLY);
+
+    if (fd < 0) {
+        strerror_r(errno, buf, sizeof(buf));
+        ALOGE("Error opening %s: %s\n", path, buf);
+        return;
+    }
+
+    len = write(fd, s, strlen(s));
+    if (len < 0) {
+        strerror_r(errno, buf, sizeof(buf));
+        ALOGE("Error writing to %s: %s\n", path, buf);
+    }
+
+    close(fd);
+}
+
+static void
+flash_buttons(void)
+{
+    sysfs_write(BUTTONS_BRIGHTNESS, "1");
+    usleep(BUTTONS_FLASH_MS*1000);
+    sysfs_write(BUTTONS_BRIGHTNESS, "0");
+}
+
 static void power_init(struct power_module *module)
 {
 }
 
 static void power_set_interactive(struct power_module *module, int on)
 {
+    ALOGD("%s on=%d old=%d\n", __func__, on, old_interactive_state);
+
+    if (on && ! old_interactive_state) {
+        flash_buttons();
+    }
+
+    old_interactive_state = on;
 }
 
 static void power_hint(struct power_module *module, power_hint_t hint,
