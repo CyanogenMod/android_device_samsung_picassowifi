fd043d87010b900bbd26b4ca89d24ab2908087c8
Christopher R. Palmer <crpalmer@gmail.com>
2015-08-15 06:25:52 -0400

    picassowifi: Switch to cm variant of insignal
    Change-Id: Ic9a17da3d0fa7f6e5f92833ec2ba728fd05bf14f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ LOCAL_PATH := device/samsung/picassowifi
 # Platform
 BOARD_VENDOR := samsung
 TARGET_BOARD_PLATFORM := exynos5
-TARGET_SLSI_VARIANT := insignal
+TARGET_SLSI_VARIANT := cm
 TARGET_SOC := exynos5420
 
 # Architecture
